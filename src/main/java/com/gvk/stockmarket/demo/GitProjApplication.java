bec4eed92277cb25ca317fda2e23721d21e3152b
DELL <DELL@DESKTOP-9VFNF4E>
2021-05-29 16:18:04 +0530

    initial project

---
+++ src/main/java/com/gvk/stockmarket/demo/GitProjApplication.java
@@ -0,0 +1,13 @@
+package com.gvk.stockmarket.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gvk/stockmarket/demo/GitProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvk.stockmarket.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
